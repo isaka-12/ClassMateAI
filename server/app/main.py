902da5c08fe7e57e13bdb390c35393ca24ecda91
isaka-12 <isakamtweve69@gmail.com>
2025-06-13 09:43:01 +0300

    FIXED IMPORT

--- server/app/main.py
+++ server/app/main.py
@@ -11,7 +11,7 @@ app = FastAPI()
 # Add CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "https://classmateai-tz.netlify.app/"],  # Add your frontend URL
+    allow_origins=["http://localhost:5173", "https://classmateai-tz.netlify.app"],  # Add your frontend URL
     allow_credentials=True,
     allow_methods=["*"],  # Allow all methods (GET, POST, PUT, DELETE, etc.)
     allow_headers=["*"],  # Allow all headers
