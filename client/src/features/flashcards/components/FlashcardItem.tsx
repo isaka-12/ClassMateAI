c7caf1e6ee2274ee05ae9297097b4cc4a49b2f4d
isaka-12 <isakamtweve69@gmail.com>
2025-06-13 09:06:03 +0300

    fixed unused imports

--- client/src/features/flashcards/components/FlashcardItem.tsx
+++ client/src/features/flashcards/components/FlashcardItem.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useImperativeHandle, forwardRef } from "react";
+import  { useState, useImperativeHandle, forwardRef } from "react";
 
 interface FlashcardItemProps {
   question: string;
